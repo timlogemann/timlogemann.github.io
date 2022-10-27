58f3046cb1b737eae812a7c7c1b3aad1d93706ab
timlogemann <tim@logemann.nl>
2022-10-27 17:53:21 +0200

    3 files modified

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log('Hello!');
+/*
+ * Normally, one would use DOMContentLoaded, but I want to be sure both the css and my face have been loaded before the "animation" gets triggered.
+ * */
+addEventListener('load', (event) => {
+  if (!document.body.classList.contains('js-disabled')) {
+    document.body.classList.add('js-animate');
+  }
+});
